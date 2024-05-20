a722e66e17315b48a1100901edacf7d3af8d3c58
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-20 10:01:28 +0545

    Auth Views Defines

--- Auth/views.py
+++ Auth/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import logout,login
 
 
@@ -15,4 +15,5 @@ def register(request):
 
 def log_out(request):
     logout(request)
+    return redirect('login')
 
