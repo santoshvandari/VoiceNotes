0b6f8e400c8fe1ec8e9ed909627a5237c55d5c8f
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-20 09:59:11 +0545

    Auth Views Defines

--- Auth/urls.py
+++ Auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Auth import views
 
 urlpatterns = [
-    path('login/',views.login,name='login'),
+    path('login/',views.log_in,name='login'),
     path('register/',views.register,name='register'),
-    path('logout/',views.logout,name='logout')
+    path('logout/',views.log_out,name='logout')
 ]
--- Auth/views.py
+++ Auth/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import logout,login
+
 
 # Create your views here.
+def log_in(request):
+    if request.method == 'POST':
+        pass
+    return render(request,'Auth/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        pass
+    return render(request,'Auth/register.html')
+
+def log_out(request):
+    logout(request)
+
