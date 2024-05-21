064e3fa6d06558a0160270a6219fd9d7cd8833aa
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-21 08:15:58 +0545

    Login View Created

--- Auth/views.py
+++ Auth/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout,login
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def log_in(request):
     if request.method == 'POST':
-        pass
+        username=request.POST['username'].strip()
+        password=request.POST['password'].strip()
+        user=User.objects.filter(username=username).first()
+        if user is not None:
+            if user.check_password(password):
+                login(request,user)
+                return redirect('home')
+            else:
+                return render(request,'Auth/login.html',{'error':'Invalid Password'})
+        else:
+            return render(request,'Auth/login.html',{'error':'Invalid Username'})
     return render(request,'Auth/login.html')
 
 def register(request):
