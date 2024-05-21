c1491e06dd7612ad397fb9658b448932c0b0c2dc
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-21 08:17:40 +0545

    Login View Created

--- Auth/views.py
+++ Auth/views.py
@@ -8,15 +8,17 @@ def log_in(request):
     if request.method == 'POST':
         username=request.POST['username'].strip()
         password=request.POST['password'].strip()
+        if username=='' or password=='':
+            return render(request,'Auth/login.html',{'error':'* All Fields are required'})
         user=User.objects.filter(username=username).first()
         if user is not None:
             if user.check_password(password):
                 login(request,user)
                 return redirect('home')
             else:
-                return render(request,'Auth/login.html',{'error':'Invalid Password'})
+                return render(request,'Auth/login.html',{'error':'* Invalid Password'})
         else:
-            return render(request,'Auth/login.html',{'error':'Invalid Username'})
+            return render(request,'Auth/login.html',{'error':'* Invalid Username'})
     return render(request,'Auth/login.html')
 
 def register(request):
