883ddd666d67c25928868d43f6a3fb100db52af3
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-22 09:18:27 +0545

    URL Defined for the HTML

--- Home/urls.py
+++ Home/urls.py
@@ -4,4 +4,7 @@ from Home import views
 urlpatterns = [
     path('',views.Home,name='home'),
     path('home/',views.Home,name='home'),    
+    path('about/',views.About,name='about'),
+    path('contact/',views.Contact,name='contact'),
+    path('privacypolicy/',views.Login,name='privacypolicy'),
 ]
