e589a58fd67ca3c99378d4b73c1731d3068950fa
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-24 08:52:09 +0545

    Contact Model Created

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=60, blank=False, null=False, default='Unknown')
+    email=models.EmailField(max_length=100, blank=False, null=False)
+    message=models.TextField(blank=False, null=False)
