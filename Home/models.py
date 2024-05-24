bdf525e1bbe5ce2f8b6b98b3c256420f4570c904
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-24 08:55:27 +0545

    Registered the Contact Model to the Admin panel

--- Home/models.py
+++ Home/models.py
@@ -5,3 +5,10 @@ class Contact(models.Model):
     name=models.CharField(max_length=60, blank=False, null=False, default='Unknown')
     email=models.EmailField(max_length=100, blank=False, null=False)
     message=models.TextField(blank=False, null=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name='Contact'
+        verbose_name_plural='Contacts'
+    
+
+
