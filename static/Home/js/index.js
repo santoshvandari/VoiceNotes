765fbb425055fef0d756634465a3e9d517d13e61
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-05-29 07:32:45 +0545

    UPdated

--- static/Home/js/index.js
+++ static/Home/js/index.js
@@ -1,3 +1,30 @@
 let saveNoteBtn=document.getElementById('SaveNoteBtn');
 let noteTitle=document.getElementById('NoteTitle');
 let noteContent=document.getElementById('NoteContent');
+console.log("Hello")
+saveNoteBtn.addEventListener('click',function(){
+    let title=noteTitle.value;
+    let content=noteContent.value;
+    console.log(title,content);
+    // make the post request to /
+    fetch('/save-note',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            title:title,
+            content:content
+        })
+    }).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        console.log(data);
+        if(data.status=='success'){
+            alert('Note Saved Successfully');
+            noteTitle.value='';
+            noteContent.value='';
+        }
+    })
+    
+})
