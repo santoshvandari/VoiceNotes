1a10238b518293c118ded7f1953810a571c2e5b5
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-01 09:37:07 +0545

    Browser Listening Feature Added

--- static/Home/js/index.js
+++ static/Home/js/index.js
@@ -1,55 +1,39 @@
-// let saveNoteBtn=document.getElementById('SaveNoteBtn');
-// let noteTitle=document.getElementById('NoteTitle');
-// let noteContent=document.getElementById('NoteContent');
-// console.log("Hello")
-// saveNoteBtn.addEventListener('click',function(){
-//     let title=noteTitle.value;
-//     let content=noteContent.value;
-//     console.log(title,content);
-//     // make the post request to /
-//     fetch('/save-note',{
-//         method:'POST',
-//         headers:{
-//             'Content-Type':'application/json'
-//         },
-//         body:JSON.stringify({
-//             title:title,
-//             content:content
-//         })
-//     }).then((response)=>{
-//         return response.json();
-//     }).then((data)=>{
-//         console.log(data);
-//         if(data.status=='success'){
-//             alert('Note Saved Successfully');
-//             noteTitle.value='';
-//             noteContent.value='';
-//         }
-//     })
-    
-// })
+(()=>{
+    console.log("Hello World")
+    let VoiceRecordBtn = document.getElementById("VoiceNoteBtn");
+    let NoteContent = document.getElementById("NoteContent");
+    console.log(VoiceRecordBtn,NoteContent);
+    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    recognition.lang = 'en-US';
 
-var speech = true; 
-        window.SpeechRecognition = window.SpeechRecognition 
-                        || window.webkitSpeechRecognition; 
-  
-        const recognition = new SpeechRecognition(); 
-        recognition.interimResults = true; 
-        // const words = document.querySelector('.words'); 
-        // words.appendChild(p); 
-        // console.log(p)
-  
-        recognition.addEventListener('result', e => { 
-            const transcript = Array.from(e.results) 
-                .map(result => result[0]) 
-                .map(result => result.transcript) 
-                .join('') 
-  
-            // document.getElementById("p").innerHTML = transcript; 
-            console.log(transcript); 
-        }); 
-          
-        if (speech == true) { 
-            recognition.start(); 
-            recognition.addEventListener('end', recognition.start); 
-        } 
+    recognition.onstart = () => {
+            console.log('Listening...');
+            VoiceRecordBtn.innerHTML = "Stop Recording";
+        };
+    recognition.onresult = (event) => {
+            const transcript = event.results[0][0].transcript;
+            NoteContent.value += transcript;
+            console.log(transcript);
+        };
+    recognition.onend = () => {
+        console.log('Stopped listening');
+        VoiceRecordBtn.innerHTML = "Start Recording";
+    };
+    
+    VoiceRecordBtn.addEventListener('click', () => {
+        if (VoiceRecordBtn.innerHTML == "Stop Recording") {
+            recognition.stop();
+            VoiceRecordBtn.innerHTML = "Start Recording";
+            return;
+        }
+        console.log("Start Recording")
+        recognition.start();
+    });
+
+
+
+
+
+
+    }
+)();
