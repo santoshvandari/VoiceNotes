2876ce51aa6b0085d545ffe688a76bca33953698
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-10 08:59:34 +0545

    Added Nepali Language Option Too

--- static/Home/js/index.js
+++ static/Home/js/index.js
@@ -2,8 +2,14 @@
     const VoiceRecordBtn = document.getElementById("VoiceNoteBtn");
     const NoteContent = document.getElementById("NoteContent");
     const RecordingStatus = document.getElementById("RecordingStatus");
+    const RecordingLanguage = document.getElementById("RecordingLangauge");
     const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
     recognition.lang = 'en-US';
+    // for the nepali audio 
+    RecordingLanguage.addEventListener('change',(e)=>{
+        console.log(RecordingLanguage.value)
+        recognition.lang=RecordingLanguage.value;
+    })
     recognition.interimResults = true; // Enable interim results for more responsive feedback
     recognition.continuous = true; // Continue recognition even after pauses
     let finalTranscript = ''; // Store final transcript
@@ -13,13 +19,15 @@
         RecordingStatus.textContent = "Listening...";
         VoiceRecordBtn.innerHTML = "Stop";
         isRecognizing = true;
+        // Add the attribute 
+        RecordingLanguage.setAttribute("disabled","disabled");
     };
 
     recognition.onresult = (event) => {
         let interimTranscript = '';
         for (let i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
-                finalTranscript += event.results[i][0].transcript.trim() + '. ';
+                finalTranscript += event.results[i][0].transcript.trim() + (recognition.lang=="np-NP"? "। " : ". ");
             } else {
                 interimTranscript += event.results[i][0].transcript;
             }
@@ -31,6 +39,8 @@
         RecordingStatus.textContent = "";
         VoiceRecordBtn.innerHTML = "Start";
         isRecognizing = false;
+        // Remove the attribute
+        RecordingLanguage.removeAttribute("disabled");
     };
 
     VoiceRecordBtn.addEventListener('click', () => {
@@ -41,4 +51,5 @@
             recognition.start();
         }
     });
+    console.log("Hello World")
 })();
